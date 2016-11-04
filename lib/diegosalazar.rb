b58d1a6f74a1b86924e91833c6d93554cc893e44
Diego Salazar <diego@greyrobot.com>
2016-11-04 18:56:19 -0400

    redid the whole damn thang

--- lib/diegosalazar.rb
+++ lib/diegosalazar.rb
@@ -6,8 +6,13 @@ require 'haml'
 
 class Diegosalazar < Sinatra::Application
   set :root, APP_ROOT
-
+  set :public_folder, File.join(APP_ROOT, "public")
+  
   get '/' do
-    haml :index
+    haml :index, layout: false
+  end
+
+  get '/resume' do
+    haml :resume, layout: false
   end
 end
