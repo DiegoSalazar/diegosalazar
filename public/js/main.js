1e90ab33f4e4356a65e54e4b28107a71cc2f0426
Diego Salazar <diego@greyrobot.com>
2016-11-05 15:35:16 -0400

    fixed github widgets

--- public/js/main.js
+++ public/js/main.js
@@ -55,7 +55,7 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar("#github-graph", "DiegoSalazar");
+    GitHubCalendar(".calendar", "DiegoSalazar");
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
     GitHubActivity.feed({ username: "DiegoSalazar", selector: "#ghfeed" });
